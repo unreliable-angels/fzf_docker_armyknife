e9235619963218e8ca626883c185862f18b7d692
Hirofumi Wakasugi <baenej@gmail.com>
2017-05-02 10:25:12 +0900

    Fix unescaped dollar sign

--- functions/fzf_docker_armyknife.fish
+++ functions/fzf_docker_armyknife.fish
@@ -10,7 +10,7 @@ function fzf_docker_armyknife -d 'fzf source of the swiss army knife for docker 
 
     # =========================================================================
     set k $k 'I want to remove all images without tag'
-    set v $v "docker rmi (docker images | grep '<none>' | awk '{print$3}')"
+    set v $v "docker rmi (docker images | grep '<none>' | awk '{print \$3}')"
     # =========================================================================
     set k $k 'I want to remove all stopped containers'
     set v $v 'docker rm (docker ps -a -q)'
